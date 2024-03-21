9c2aa4c65770c6791f218be2cb24a6a2dba39f3c
PaulOhNoes <paulosorio1996@gmail.com>
2024-03-21 13:35:52 -0400

    rename variables

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
 import os
 from datetime import datetime
 
-print("start test script")
-
 def make_log(date): 
-  newlog = open(f"newlogs/{date}.log", "w")
+  newlog = open(f"testLogs/{date}.log", "w")
   newlog.close()
 
 def sort_log(string):
@@ -16,27 +14,32 @@ def sort_log(string):
 
   return datetime.strptime(date, '%Y_%m_%d').timestamp() + section
 
-logs_dir_parth = os.path.join(os.getcwd(), "logs")
-logs_dir = os.listdir(logs_dir_parth)
-logs_dir.sort(key=sort_log, reverse=True)
+def main():
+  logs_dir_path = os.path.join(os.getcwd(), "logs")
+  logs_dir = os.listdir(logs_dir_path)
+  logs_dir.sort(key=sort_log, reverse=True)
 
-for log in logs_dir:
-  print(log)
+  test_logs_dir_path = os.path.join(os.getcwd(), "testlogs")
 
-for filename in logs_dir:
-  new_logs_dir_path = os.path.join(os.getcwd(), "newlogs")
-  new_log_file_path = os.path.join(new_logs_dir_path, f"{filename[0:10]}.log")
-  if(not os.path.exists(new_log_file_path)):
-    file = open(new_log_file_path, "x")
-    file.close()
-  
-  log = open(new_log_file_path, "a")
+  # Make testlogs directory if it does not exist
+  if(not os.path.exists(test_logs_dir_path)):
+    os.mkdir(test_logs_dir_path)
 
-  with open(os.path.join(logs_dir_parth, filename), "r") as file:
-    log.write(f"--- merging {filename} ---\n")
-    log.writelines(file.readlines())
-    file.close()
+  for filename in logs_dir:
+    new_log_file_path = os.path.join(test_logs_dir_path, f"{filename[0:10]}.log")
+    if(not os.path.exists(new_log_file_path)):
+      file = open(new_log_file_path, "x")
+      file.close()
+    
+    log = open(new_log_file_path, "a")
 
-  log.close()
+    with open(os.path.join(logs_dir_path, filename), "r") as file:
+      log.write(f"--- merging {filename} ---\n")
+      log.writelines(file.readlines())
+      file.close()
 
-print("end script")
+    log.close()
+
+print("start test script")
+main()
+print("end test script")
